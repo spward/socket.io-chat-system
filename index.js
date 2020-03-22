4fe306b9c0c0e2e830f857df2cedc64817df5bab
spward <spward@coastal.edu>
2020-03-21 23:41:49 -0400

    Updated Chat System

--- index.js
+++ index.js
@@ -3,22 +3,65 @@ var app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 const PORT = process.env.PORT || 3000;
-
 app.use(express.static(__dirname + "/public"));
 
-io.on("connection", socket => {
-  // Displays if a user connects
-  socket.broadcast.emit("chat-message", "a user connected!");
-  console.log("a user connected");
+var numUsers = 0;
 
-  // when someone disconnects
-  socket.on("disconnect", () => {
-    // Displays if a user disconnects
-    console.log("user disconnected");
-    socket.broadcast.emit("chat-message", "a user disconnected!");
+io.on("connection", socket => {
+  var addedUser = false;
+
+  // when the client emits 'new message', this listens and executes
+  socket.on("new-message", data => {
+    // we tell the client to execute 'new message'
+    socket.broadcast.emit("new-message", {
+      username: socket.username,
+      message: data
+    });
   });
-  socket.on("chat-message", msg => {
-    io.emit("chat-message", msg);
+
+  // when the client emits 'add user', this listens and executes
+  socket.on("add-user", username => {
+    if (addedUser) return;
+
+    // we store the username in the socket session for this client
+    socket.username = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit("login", {
+      numUsers: numUsers
+    });
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit("user-joined", {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
+
+  // when the client emits 'typing', we broadcast it to others
+  socket.on("typing", () => {
+    socket.broadcast.emit("typing", {
+      username: socket.username
+    });
+  });
+
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on("stopped-typing", () => {
+    socket.broadcast.emit("stopped-typing", {
+      username: socket.username
+    });
+  });
+
+  // when the user disconnects.. perform this
+  socket.on("disconnect", () => {
+    if (addedUser) {
+      --numUsers;
+
+      // echo globally that this client has left
+      socket.broadcast.emit("user-left", {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    }
   });
 });
 
